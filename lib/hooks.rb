6c7c273fcfcb19ae14c35bb67ee889638b1720e3
delba <delba@users.noreply.github.com>
2015-02-16 23:17:06 -0500

    Add hooks

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,5 +1,43 @@
 require "hooks/version"
 
 module Hooks
-  # Your code goes here...
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def method_missing(method, *args)
+      unless /\A(before|after|around)_(.+)\z/ =~ method
+        super
+      end
+
+      send "add_#{$1}_hook", $2, args
+    end
+
+  private
+
+    def add_before_hook(method, hooks)
+      prepend_method method do
+        hooks.each { |hook| send hook }
+        super()
+      end
+    end
+
+    def add_after_hook(method, hooks)
+      prepend_method method do
+        super()
+        hooks.each { |hook| send hook }
+      end
+    end
+
+    def add_around_hook(method, hooks)
+      prepend_method method do
+        send hooks[0] { super() }
+      end
+    end
+
+    def prepend_method(method, &block)
+      prepend Module.new { define_method(method, &block) }
+    end
+  end
 end
--- test/test_hooks.rb
+++ test/test_hooks.rb
@@ -1,11 +1,66 @@
 require 'minitest_helper'
 
-class TestHooks < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Hooks::VERSION
+class Person
+  include Hooks
+
+  before_talk :listen
+  around_party :smile
+  after_sleep :wake_up
+
+  attr_reader :before, :around, :after
+
+  def initialize
+    @before = []
+    @around = []
+    @after  = []
   end
 
-  def test_it_does_something_useful
-    assert false
+  def talk
+    @before.push :talk
+  end
+
+  def party
+    @around.push :party
+  end
+
+  def sleep
+    @after.push :sleep
+  end
+
+private
+
+  def listen
+    @before.push :listen
+  end
+
+  def smile
+    @around.push :smile_before
+    yield
+    @around.push :smile_after
+  end
+
+  def wake_up
+    @after.push :wake_up
+  end
+end
+
+class TestHooks < Minitest::Test
+  def setup
+    @person = Person.new
+  end
+
+  def test_before_hook
+    @person.talk
+    assert_equal [:listen, :talk], @person.before
+  end
+
+  def test_around_hook
+    @person.party
+    assert_equal [:smile_before, :party, :smile_after], @person.around
+  end
+
+  def test_after_hook
+    @person.sleep
+    assert_equal [:sleep, :wake_up], @person.after
   end
 end
